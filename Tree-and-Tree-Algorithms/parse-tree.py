c534f6d5e257406e9d4295a349e623d2846bd6f2
Adnan Tahir <adnantabda@gmail.com>
2024-10-21 14:15:01 +0300

    Add: tree parsing

---
+++ Tree-and-Tree-Algorithms/parse-tree.py
@@ -0,0 +1,75 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, value):
+        self.items.append(value)
+    
+    def pop(self):
+        return self.items.pop()
+
+
+class BinaryTree():
+    def __init__(self, root):
+        self.key = root
+        self.left_child = None
+        self.right_child = None
+    
+    def insert_left(self, new_node):
+        if self.left_child == None:
+            self.left_child = BinaryTree(new_node)
+        else:
+            t = BinaryTree(new_node)
+            t.left_child = self.left_child
+            self.left_child = t
+
+    def insert_right(self, new_node):
+        if self.right_child == None:
+            self.right_child = BinaryTree(new_node)
+        else:
+            t = BinaryTree(new_node)
+            t.right_child = self.right_child
+            self.right_child = t
+    
+    def get_right_child(self):
+        return self.right_child
+    
+    def get_left_child(self):
+        return self.left_child
+
+    def set_root_val(self, obj):
+        self.key = obj
+
+    def get_root_val(self):
+        return self.key
+
+def build_parse_tree(fp_exp):
+    fp_list = fp_exp.split()
+    p_stack = Stack()
+    e_tree = BinaryTree('')
+    p_stack.push(e_tree)
+    current_tree = e_tree
+    for i in fp_list:
+        if i =='(':
+            current_tree.insert_left('')
+            p_stack.push(current_tree)
+            current_tree = current_tree.get_left_child()
+        elif i not in ['+', '-', '*', '/', ')']:
+            current_tree.set_root_val(int(i))
+            parent = p_stack.pop()
+            current_tree = parent
+        elif i in ['+', '-', '*', '/']:
+            current_tree.set_root_val((i))
+            current_tree.insert_right('')
+            p_stack.push(current_tree)
+            current_tree = current_tree.get_right_child()
+        elif i == ')':
+            current_tree = p_stack.pop()
+        else:
+            raise ValueError
+    
+    return e_tree
+
+
+pt = build_parse_tree("( ( 10 + 5 ) * 3 )")
+print(pt.get_left_child().get_left_child().get_root_val())
